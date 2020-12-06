3f0f29950b5ee997bdebe512951bf951b6ce9516
zabtani <zabtani@gmail.com>
2020-12-06 17:12:56 +0200

    ui/engine update

--- engine.js
+++ engine.js
@@ -2,23 +2,34 @@ const OPTIONAL_COLORS = ['black', 'blue', 'red', 'green', 'yellow'];
 let LVL = 1;
 let LVL_UNSOLVED;
 let justEntered = true;
+const COMPLIMENTS = [
+  'good memory!',
+  'genius!',
+  'champion!',
+  'elephant!',
+  'wooow!',
+  'nice!',
+  'amazing!',
+];
 
 if (justEntered) {
-  UI.gameNotification('welcome');
+  UI.welcomeMSG();
   justEntered = false;
 }
+
 const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-const generateCompliment = (comments) => {
-  return comments[randomInt(0, comments.length - 1)];
+const generateCompliment = () => {
+  return COMPLIMENTS[randomInt(0, COMPLIMENTS.length - 1)];
 };
 
 const lostLVL = () => {
   if (LVL > 1) {
     LVL--;
   }
-  UI.gameNotification('fail');
+
+  UI.wrongMSG();
   setTimeout(() => {
     startRound();
   }, 2000);
@@ -26,52 +37,53 @@ const lostLVL = () => {
 
 const wonLVL = () => {
   LVL++;
-  UI.revealStartBtn();
-  UI.gameNotification('success');
+  UI.titleOnStart();
+  UI.successMSG();
+};
+
+const rightAnswer = (section) => {
+  const commpliment = generateCompliment();
+  UI.sectionSolved(commpliment, section);
+  LVL_UNSOLVED--;
+  if (LVL_UNSOLVED === 0) {
+    wonLVL();
+  }
 };
 
 const presentOptionalAnswers = (section) => {
-  const currentSection = sections[section];
-  const sectionAnswer = currentSection.style.backgroundColor;
-  const rightAnswer = () => {
-    UI.sectionRandomCompliment(currentSection);
-    LVL_UNSOLVED--;
-    if (LVL_UNSOLVED === 0) {
-      wonLVL();
-    }
-  };
+  const sectionAnswer = section.style.backgroundColor;
   for (color of OPTIONAL_COLORS) {
-    let colorOption = UI.createSectionAnswerBtn(currentSection, color);
+    let colorOption = UI.createSectionOptionBtn(section, color);
     colorOption.addEventListener(
       'click',
-      sectionAnswer === color ? rightAnswer : lostLVL
+      sectionAnswer === color ? rightAnswer.bind(this, section) : lostLVL
     );
   }
 };
 
 const hideSections = () => {
   for (section = 0; section < sections.length; section++) {
-    presentOptionalAnswers(section);
-    UI.hideSectionContent(sections[section]);
+    presentOptionalAnswers(sections[section]);
+    UI.hideSectionColor(sections[section]);
   }
 };
 
 const startRound = () => {
-  LVL_UNSOLVED = sections.length;
   buildSections();
+  LVL_UNSOLVED = sections.length;
   setTimeout(() => {
     hideSections();
   }, 2000);
 };
 
 const buildSections = () => {
-  UI.lvlTitleView();
+  UI.titleOnLvl();
   for (section = 0; section < LVL; section++) {
-    const sectionColor = `${
+    const optionalColor = `${
       OPTIONAL_COLORS[randomInt(0, OPTIONAL_COLORS.length - 1)]
     }`;
-    UI.createSection(sectionColor);
-  } 
+    UI.createSection(optionalColor);
+  }
 };
 
 startBtn.addEventListener('click', startRound);
--- ui.js
+++ ui.js
@@ -2,62 +2,48 @@ const mainContainer = document.getElementById('main-container');
 const titleContainer = document.getElementById('title-container');
 const sections = mainContainer.childNodes;
 const startBtn = document.getElementById('start-btn');
-const compliments = [
-  'good memory!',
-  'genius!',
-  'champion!',
-  'elephant!',
-  'wooow!',
-  'nice!',
-  'amazing!',
-];
 
 const UI = {
-  gameNotification: (type) => {
-    let notification;
-    switch (type) {
-      case 'fail':
-        notification = `<span class='red'><span class='big-letters'>wrong answer!</span><br><br> back to LVL ${LVL} </span>`;
-        break;
-      case 'success':
-        notification = `<span class='green'><span class='big-letters'>good job!</span><br><br> press start for round ${LVL}</span> `;
-        break;
-      case 'welcome':
-        notification =
-          'welcome. please follow the color and recall which is where. start game when ready.';
-    }
-    mainContainer.innerHTML = notification;
+  wrongMSG: () => {
+    mainContainer.innerHTML = `wrong answer! back to LVL ${LVL}`;
+  },
+  successMSG: () => {
+    mainContainer.innerHTML = `good job! press start for round ${LVL}`;
   },
 
-  lvlTitleView: () => {
+  welcomeMSG: () => {
+    mainContainer.innerHTML =
+      'welcome. please follow the colors and recall which is where. start game when ready.';
+  },
+
+  titleOnLvl: () => {
     mainContainer.innerHTML = '';
     startBtn.style.display = 'none';
     titleContainer.innerHTML = `LVL ${LVL}`;
   },
-  sectionRandomCompliment: (section) => {
-    section.innerHTML = generateCompliment(compliments);
-  },
-
-  revealStartBtn: () => {
+  titleOnStart: () => {
     titleContainer.innerHTML = '';
     titleContainer.appendChild(startBtn);
     startBtn.style.display = 'block';
   },
-  createSectionAnswerBtn: (section, color) => {
+  sectionSolved: (compliment, section) => {
+    section.innerHTML = compliment;
+  },
+
+  createSectionOptionBtn: (section, OptionalColor) => {
     const colorBtn = document.createElement('button');
     colorBtn.setAttribute(`class`, `answer-cube`);
-    colorBtn.style.backgroundColor = color;
+    colorBtn.style.backgroundColor = OptionalColor;
     section.appendChild(colorBtn);
     return colorBtn;
   },
 
-  createSection: (OptionalColor) => {
+  createSection: (randomColor) => {
     const section = document.createElement('div');
-    const sectionColor = OptionalColor;
-    section.style.backgroundColor = sectionColor;
+    section.style.backgroundColor = randomColor;
     mainContainer.appendChild(section);
   },
-  hideSectionContent: (sectionContent) => {
-    sectionContent.style.backgroundColor = 'transparent';
+  hideSectionColor: (section) => {
+    section.style.backgroundColor = 'transparent';
   },
 };
